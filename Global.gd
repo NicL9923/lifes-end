e3f5cb72fe614a38d7b7ef95fa2e3d780e2bafed
Nicolas Layne <nicl9923@gmail.com>
2021-12-23 14:30:44 -0600

    Adding volume slider

--- Global.gd
+++ Global.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var player: Player
+var audioVolume: int
--- MainMenu.gd
+++ MainMenu.gd
@@ -1,16 +1,34 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/NewGameButton.grab_focus()
+	$SettingsContainer.visible = false
+	$LoadGameContainer.visible = false
+	$MainMenuContainer/MainMenuVBox/NewGameButton.grab_focus()
 
 func _on_NewGameButton_pressed():
 	get_tree().change_scene("res://CharacterCreation.tscn")
 
 func _on_LoadGameButton_pressed():
-	pass
+	$MainMenuContainer.visible = false
+	$LoadGameContainer.visible = true
 
 func _on_SettingsButton_pressed():
-	pass
+	$MainMenuContainer.visible = false
+	$SettingsContainer.visible = true
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+
+func _on_S_GoBackBN_pressed():
+	$SettingsContainer.visible = false
+	$MainMenuContainer.visible = true
+
+
+func _on_LG_GoBackBN_pressed():
+	$LoadGameContainer.visible = false
+	$MainMenuContainer.visible = true
+
+
+func _on_HSlider_value_changed(value):
+	Global.audioVolume = value as int
