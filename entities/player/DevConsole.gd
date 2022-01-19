7e1686fe147c3f3e6d2f319f1eb22770d16fe4a2
Nicolas Layne <nicl9923@gmail.com>
2022-01-19 16:39:14 -0600

    Added tilemap to main world

--- entities/player/DevConsole.gd
+++ entities/player/DevConsole.gd
@@ -28,11 +28,13 @@ func execute_dev_commands():
 	var cmdTxt = Array($ColorRect/LineEdit.text.split(' '))
 		
 	#TODO: god mode (no dmg + unlimited ammo), ammo, other resources
-	if cmdTxt[0] == "metal" and cmdTxt[1] != null:
+	if cmdTxt[0] == "metal" and cmdTxt[1] != null: #metal 5
 		Global.playerBaseMetal += int(cmdTxt[1])
-	elif cmdTxt[0] == "health" and cmdTxt[1] != null:
+	elif cmdTxt[0] == "health" and cmdTxt[1] != null: #health 100
 		Global.playerHealth = int(cmdTxt[1])
-	elif cmdTxt[0] == "load_scene" and cmdTxt[1] != null:
+	elif cmdTxt[0] == "load_scene" and cmdTxt[1] != null: #load_scene MainWorld
 		get_tree().change_scene("res://" + cmdTxt[1] + ".tscn")
+	elif cmdTxt[0] == "teleport" and cmdTxt[1] != null and cmdTxt[2] != null: #teleport 20 30
+		Global.player.global_position = Vector2(int(cmdTxt[1]), int(cmdTxt[2]))
 	else:
 		$ColorRect/LineEdit.placeholder_text = "Error: Invalid command"
