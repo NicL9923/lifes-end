e70b8a1245bc78f5e9ae816f49ab2fd60dc9ecd0
Nicolas Layne <nicl9923@gmail.com>
2022-01-29 16:29:05 -0600

    Playing around with rotating planet and mapping Sprite3Ds to it

--- SystemMap.gd
+++ SystemMap.gd
@@ -1,14 +1,33 @@
 extends Spatial
 
 var currently_selected_planet := 2
+var player_rotate_sensitivity := 1
+var mouse_pressed := false
 
-
-func _ready():
-	pass
 
 func _process(_delta):
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().change_scene("res://MainGame.tscn")
+	
+	map_icons_to_planet()
+
+func _input(event):
+	# Handle player dragging/rotating planet
+	if event is InputEventMouseButton:
+		mouse_pressed = event.pressed
+	if event is InputEventMouseMotion and mouse_pressed:
+		$Planet.rotation_degrees = Vector3($Planet.rotation_degrees.x + (event.relative.x * player_rotate_sensitivity), $Planet.rotation_degrees.y + (event.relative.y * player_rotate_sensitivity), $Planet.rotation_degrees.z)
+
+func map_icons_to_planet():
+	# TODO: map icons to planets
+		# Check player and npc bases for planet, and if it's the current one, map those icons (Sprite3D)
+	var coords = $Planet.get_coords_from_lat_long(60, 60)
+	print(coords)
+	
+	var test_sprite = Sprite3D.new()
+	test_sprite.texture = load("res://objects/ship.png")
+	$Planet.add_child(test_sprite)
+	test_sprite.translation = coords
 
 func update_thumbnail_highlight_pos():
 	$UI/Planet_Thumbnail_Container/Highlight.rect_position.x = currently_selected_planet * 32 + 2
@@ -17,7 +36,7 @@ func display_planet(planet_index):
 	$UI/PlanetName_Label.text = Global.planets[planet_index]
 	update_thumbnail_highlight_pos()
 	
-	# TODO: load 3D planet mesh
+	# TODO: apply planet's specific shader to mesh
 
 func _on_Mercury_Area_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed:
