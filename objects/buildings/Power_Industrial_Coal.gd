c5981ff3dd8e340f0f27733ae893854f0a1c3278
Nicolas Layne <nicl9923@gmail.com>
2022-02-24 21:18:24 -0600

    Energy buildings now produce a constant amount of power as opposed to some additional amt each day

--- objects/buildings/Power_Industrial_Coal.gd
+++ objects/buildings/Power_Industrial_Coal.gd
@@ -1,17 +1,20 @@
 extends Building
 
-export var energy_produced_per_day := 15
+export var energy_produced := 20
 export var pollution_produced_per_day := 0.1
 
 
 func _init():
 	cost_to_build = 15
 	bldg_name = "Coal Power Plant"
-	bldg_desc = "Produces " + str(energy_produced_per_day) + " Energy and " + str(pollution_produced_per_day) + " Pollution per day"
+	bldg_desc = "Produces " + str(energy_produced) + " Energy, and " + str(pollution_produced_per_day) + " Pollution per day"
 
 func _ready():
 	connect_to_daynight_cycle()
+	Global.playerResources.energy += energy_produced
+
+func _exit_tree():
+	Global.playerResources.energy -= energy_produced
 
 func handle_new_day():
-	add_energy(energy_produced_per_day)
 	add_pollution(pollution_produced_per_day)
