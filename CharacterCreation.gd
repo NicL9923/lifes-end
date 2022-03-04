85162b7f1297172fcd775d8ffda3929d64219a4b
Nicolas Layne <nicl9923@gmail.com>
2022-03-04 14:09:24 -0600

    Updated MainMenu and CharCreation to use new buttons

--- CharacterCreation.gd
+++ CharacterCreation.gd
@@ -3,35 +3,44 @@ extends Control
 const MAX_STAT_VALUE := 10
 export var remaining_attr_pts := 0
 
-onready var remaining_pts_lbl = $Lbl_RemPnts
-onready var cmdr_spinbox = $Cmdr_HBox/Cmdr_SpinBox
-onready var biol_spinbox = $Bio_HBox/Biol_SpinBox
-onready var engr_spinbox = $Engr_HBox/Engr_SpinBox
-onready var doc_spinbox = $Doc_HBox/Doc_SpinBox
-onready var launch_btn = $"Launch Button"
+onready var remaining_pts_lbl := $Lbl_RemPnts
+onready var cmdr_pts := $Cmdr_HBox/Pts_Label
+onready var bio_pts := $Bio_HBox/Pts_Label
+onready var engr_pts := $Engr_HBox/Pts_Label
+onready var doc_pts := $Doc_HBox/Pts_Label
+onready var launch_btn := $Launch_Btn
 
 var cur_cmdr: int
-var cur_biol: int
+var cur_bio: int
 var cur_engr: int
 var cur_doc: int
 
-# TODO: Handle bug with remaining amts going bonkers if you manually edit values...or
-# just replace the spin boxes entirely w/ a btn-only system
 
 func _ready():
+	$FadeTransition.visible = true
 	$AnimationPlayer.play("fade_in")
 	
-	launch_btn.disabled = true
-	update_remaining_points_lbl()
+	$Cmdr_HBox/ArrowCont/Up_Arrow.connect("button_pressed", self, "_on_Arrow_button_pressed", [true, "cmdr"])
+	$Cmdr_HBox/ArrowCont/Down_Arrow.connect("button_pressed", self, "_on_Arrow_button_pressed", [false, "cmdr"])
+	$Bio_HBox/ArrowCont/Up_Arrow.connect("button_pressed", self, "_on_Arrow_button_pressed", [true, "bio"])
+	$Bio_HBox/ArrowCont/Down_Arrow.connect("button_pressed", self, "_on_Arrow_button_pressed", [false, "bio"])
+	$Engr_HBox/ArrowCont/Up_Arrow.connect("button_pressed", self, "_on_Arrow_button_pressed", [true, "engr"])
+	$Engr_HBox/ArrowCont/Down_Arrow.connect("button_pressed", self, "_on_Arrow_button_pressed", [false, "engr"])
+	$Doc_HBox/ArrowCont/Up_Arrow.connect("button_pressed", self, "_on_Arrow_button_pressed", [true, "doc"])
+	$Doc_HBox/ArrowCont/Down_Arrow.connect("button_pressed", self, "_on_Arrow_button_pressed", [false, "doc"])
 	
-	cur_cmdr = cmdr_spinbox.value
+	launch_btn.disabled = true
+	
+	cur_cmdr = int(cmdr_pts.text)
 	Global.playerStats.cmdr = cur_cmdr
-	cur_biol = biol_spinbox.value
-	Global.playerStats.biol = cur_biol
-	cur_engr = engr_spinbox.value
+	cur_bio = int(bio_pts.text)
+	Global.playerStats.biol = cur_bio
+	cur_engr = int(engr_pts.text)
 	Global.playerStats.engr = cur_engr
-	cur_doc = doc_spinbox.value
+	cur_doc = int(doc_pts.text)
 	Global.playerStats.doc = cur_doc
+	
+	update_points_labels()
 
 func check_all_fields_completed():
 	if Global.playerName.length() > 0 and Global.playerName.length() <= 20 and remaining_attr_pts == 0:
@@ -39,63 +48,74 @@ func check_all_fields_completed():
 	else:
 		launch_btn.disabled = true
 
-func update_remaining_points_lbl():
-	remaining_pts_lbl.text = "Points remaining: " + String(remaining_attr_pts)
+func update_points_labels():
+	remaining_pts_lbl.text = "Points remaining: " + str(remaining_attr_pts)
+	cmdr_pts.text = str(cur_cmdr)
+	bio_pts.text = str(cur_bio)
+	engr_pts.text = str(cur_engr)
+	doc_pts.text = str(cur_doc)
 	check_all_fields_completed()
 
-func _on_Cmdr_SpinBox_value_changed(value):
-	if remaining_attr_pts == 0 and value > cur_cmdr:
-		cmdr_spinbox.value = cur_cmdr
+func _on_Cmdr_value_changed(new_value):
+	if new_value < 0 or new_value > 10:
+		return
+	if remaining_attr_pts == 0 and new_value > cur_cmdr:
+		cmdr_pts.text = str(cur_cmdr)
 		return
 	
-	remaining_attr_pts -= (value - cur_cmdr)
-	Global.playerStats.cmdr = value
-	cur_cmdr = value
-	
-	update_remaining_points_lbl()
+	remaining_attr_pts -= (new_value - cur_cmdr)
+	Global.playerStats.cmdr = new_value
+	cur_cmdr = new_value
 
-func _on_Biol_SpinBox_value_changed(value):
-	if remaining_attr_pts == 0 and value > cur_biol:
-		biol_spinbox.value = cur_biol
+func _on_Bio_value_changed(new_value):
+	if new_value < 0 or new_value > 10:
+		return
+	if remaining_attr_pts == 0 and new_value > cur_bio:
+		bio_pts.text = str(cur_bio)
 		return
 	
-	remaining_attr_pts -= (value - cur_biol)
-	Global.playerStats.biol = value
-	cur_biol = value
-	
-	update_remaining_points_lbl()
+	remaining_attr_pts -= (new_value - cur_bio)
+	Global.playerStats.biol = new_value
+	cur_bio = new_value
 
-func _on_Engr_SpinBox_value_changed(value):
-	if remaining_attr_pts == 0 and value > cur_engr:
-		engr_spinbox.value = cur_engr
+func _on_Engr_value_changed(new_value):
+	if new_value < 0 or new_value > 10:
+		return
+	if remaining_attr_pts == 0 and new_value > cur_engr:
+		engr_pts.text = str(cur_engr)
 		return
 	
-	remaining_attr_pts -= (value - cur_engr)
-	Global.playerStats.engr = value
-	cur_engr = value
-	
-	update_remaining_points_lbl()
+	remaining_attr_pts -= (new_value - cur_engr)
+	Global.playerStats.engr = new_value
+	cur_engr = new_value
 
-func _on_Doc_SpinBox_value_changed(value):
-	if remaining_attr_pts == 0 and value > cur_doc:
-		doc_spinbox.value = cur_doc
+func _on_Doc_value_changed(new_value):
+	if new_value < 0 or new_value > 10:
+		return
+	if remaining_attr_pts == 0 and new_value > cur_doc:
+		doc_pts.text = str(cur_doc)
 		return
 	
-	remaining_attr_pts -= (value - cur_doc)
-	Global.playerStats.doc = value
-	cur_doc = value
-	
-	update_remaining_points_lbl()
-
-
-func _on_Launch_Button_pressed():
-	$AnimationPlayer.play("fade_out")
-	yield($AnimationPlayer, "animation_finished")
-	get_tree().change_scene("res://cutscenes/IntroCinematic.tscn")
-
+	remaining_attr_pts -= (new_value - cur_doc)
+	Global.playerStats.doc = new_value
+	cur_doc = new_value
 
 func _on_LineEdit_text_changed(new_text):
 	if new_text.length() > 0 and new_text.length() <= 20:
 		Global.playerName = new_text
 	
 	check_all_fields_completed()
+
+func _on_Launch_Btn_button_pressed():
+	$AnimationPlayer.play("fade_out")
+	yield($AnimationPlayer, "animation_finished")
+	get_tree().change_scene("res://cutscenes/IntroCinematic.tscn")
+
+func _on_Arrow_button_pressed(isUp: bool, type: String):
+	match type:
+		"cmdr": _on_Cmdr_value_changed(cur_cmdr + (1 if isUp else -1))
+		"bio": _on_Bio_value_changed(cur_bio + (1 if isUp else -1))
+		"engr": _on_Engr_value_changed(cur_engr + (1 if isUp else -1))
+		"doc": _on_Doc_value_changed(cur_doc + (1 if isUp else -1))
+	
+	update_points_labels()
